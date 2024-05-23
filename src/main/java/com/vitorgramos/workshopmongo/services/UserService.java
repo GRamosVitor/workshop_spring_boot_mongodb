1f9645e12a3845038da6793633d5c3fcaff25ea4
Vitor Ramos <gramosvitor@gmail.com>
2024-05-23 15:26:12 -0300

    User insertion with POST

--- src/main/java/com/vitorgramos/workshopmongo/services/UserService.java
+++ src/main/java/com/vitorgramos/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vitorgramos.workshopmongo.domain.User;
+import com.vitorgramos.workshopmongo.dto.UserDTO;
 import com.vitorgramos.workshopmongo.repository.UserRepository;
 import com.vitorgramos.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fronDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
